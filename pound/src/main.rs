d554f6121408f13ee78878dfde5f253398726686
likanug <1452930742@qq.com>
2023-08-27 14:57:57 +0800

    添加raw模式的输入后自动回车

--- pound/src/main.rs
+++ pound/src/main.rs
@@ -1,6 +1,10 @@
 use std::io;
 use std::io::Read;
+
+use crossterm::terminal;
 fn main() {
+    terminal::enable_raw_mode().expect("could not turn on Raw mode");
     let mut buf = [0; 1];
-    while io::stdin().read(&mut buf).expect("Failed to read line") == 1 {}
+    //按q退出，b'q'表示q是一个字节
+    while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
 }
