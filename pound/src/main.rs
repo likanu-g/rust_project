e0508d15fc1ffc3bf9b0476cab196464c4504e88
likanug <1452930742@qq.com>
2023-08-27 15:14:45 +0800

    添加字符输出逻辑和部分注释

--- pound/src/main.rs
+++ pound/src/main.rs
@@ -12,9 +12,19 @@ impl Drop for CleanUp {
 }
 
 fn main() {
+    let _clean_up = CleanUp;
     terminal::enable_raw_mode().expect("could not turn on Raw mode");
     let mut buf = [0; 1];
     //按q退出，b'q'表示q是一个字节
-    while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
-    panic!();
+    while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {
+        let character = buf[0] as char;
+        //判断是否是控制字符，因为控制字符在ASCII码中无法打印，
+        //ASCII码0-31和127为为控制字符
+        //ASCII码32-126为可打印字符
+        if character.is_control() {
+            println!("{}\r", character as u8)
+        } else {
+            println!("{}\r", character)
+        }
+    }
 }
