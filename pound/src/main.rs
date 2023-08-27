b4c9f05dd4437d49e2a517cf4ebd5ba48244b454
likanug <1452930742@qq.com>
2023-08-27 16:37:03 +0800

    使用Editor和Reader结构体重构项目

--- pound/src/main.rs
+++ pound/src/main.rs
@@ -7,31 +7,60 @@ struct CleanUp;
 
 impl Drop for CleanUp {
     fn drop(&mut self) {
-        terminal::disable_raw_mode().expect("Could not disable raw mode")
+        terminal::disable_raw_mode().expect("Unable to disable raw mode")
+    }
+}
+
+struct Reader;
+
+impl Reader {
+    fn read_key(&self) -> std::io::Result<KeyEvent> {
+        loop {
+            if event::poll(Duration::from_millis(500))? {
+                if let Event::Key(event) = event::read()? {
+                    println!("{:?}\r", event);
+                    return Ok(event);
+                }
+            } else {
+                println!("No input yet\r");
+            }
+        }
+    }
+}
+
+struct Editor {
+    reader: Reader,
+}
+
+impl Editor {
+    fn new() -> Self {
+        Self { reader: Reader }
+    }
+
+    fn process_keypress(&self) -> std::io::Result<bool> {
+        match self.reader.read_key()? {
+            KeyEvent {
+                code: KeyCode::Char('c'),
+                modifiers: event::KeyModifiers::CONTROL,
+                kind: _,
+                state: _,
+            } => return Ok(false),
+            _ => {}
+        }
+        Ok(true)
+    }
+
+    fn run(&self) -> std::io::Result<bool> {
+        self.process_keypress()
     }
 }
 
 fn main() -> std::io::Result<()> {
     let _clean_up = CleanUp;
     terminal::enable_raw_mode()?;
-
-    loop {
-        if event::poll(Duration::from_millis(500))? {
-            if let Event::Key(event) = event::read()? {
-                match event {
-                    KeyEvent {
-                        code: KeyCode::Char('q'),
-                        modifiers: event::KeyModifiers::NONE,
-                        kind: _,
-                        state: _,
-                    } => break,
-                    _ => {}
-                }
-                println!("{:?}\r", event);
-            };
-        } else {
-            println!("No input yet\r");
-        }
-    }
+    /* modify */
+    let editor = Editor::new();
+    while editor.run()? {}
+    /* end */
     Ok(())
 }
