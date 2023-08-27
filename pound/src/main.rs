affabb59c766c928cb864e4780fe32f486408122
likanug <1452930742@qq.com>
2023-08-27 15:03:16 +0800

    添加退出raw模式后的异常测试

--- pound/src/main.rs
+++ pound/src/main.rs
@@ -2,9 +2,19 @@ use std::io;
 use std::io::Read;
 
 use crossterm::terminal;
+
+struct CleanUp;
+
+impl Drop for CleanUp {
+    fn drop(&mut self) {
+        terminal::disable_raw_mode().expect("Could not disable raw mode")
+    }
+}
+
 fn main() {
     terminal::enable_raw_mode().expect("could not turn on Raw mode");
     let mut buf = [0; 1];
     //按q退出，b'q'表示q是一个字节
     while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
+    panic!();
 }
