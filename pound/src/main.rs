c19e2d33b45c5c158d53906b5f29d2bd17c7aad7
likanug <1452930742@qq.com>
2023-08-27 15:37:33 +0800

    增加超时读取判断

--- pound/src/main.rs
+++ pound/src/main.rs
@@ -1,6 +1,8 @@
 use crossterm::event::{self, Event, KeyCode, KeyEvent};
 use crossterm::terminal;
 
+use std::time::Duration;
+
 struct CleanUp;
 
 impl Drop for CleanUp {
@@ -14,17 +16,21 @@ fn main() {
     terminal::enable_raw_mode().expect("could not turn on Raw mode");
 
     loop {
-        if let Event::Key(event) = event::read().expect("failed to read line") {
-            match event {
-                KeyEvent {
-                    code: KeyCode::Char('q'),
-                    modifiers: event::KeyModifiers::NONE,
-                    kind: _,
-                    state: _,
-                } => break,
-                _ => {}
-            }
-            println!("{:?}\r", event);
-        };
+        if event::poll(Duration::from_millis(500)).expect("Error") {
+            if let Event::Key(event) = event::read().expect("failed to read line") {
+                match event {
+                    KeyEvent {
+                        code: KeyCode::Char('q'),
+                        modifiers: event::KeyModifiers::NONE,
+                        kind: _,
+                        state: _,
+                    } => break,
+                    _ => {}
+                }
+                println!("{:?}\r", event);
+            };
+        } else {
+            println!("No input yet\r");
+        }
     }
 }
